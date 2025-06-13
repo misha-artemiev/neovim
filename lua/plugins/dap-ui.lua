5ed0020581fc6712bd174e4c823a49858d02af73
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2025-06-13 19:35:33 +0100

    update

--- lua/plugins/dap-ui.lua
+++ lua/plugins/dap-ui.lua
@@ -2,9 +2,24 @@ return {
   "rcarriga/nvim-dap-ui",
   lazy = false,
   dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
-  opts = {},
-  keys = {
-    { "<leader>dp", function() require("dapui").toggle() end, silent = true },
-  },
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
+    dapui.setup()
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+  end,
   enabled = true,
 }
---
+++ lua/plugins/dashboard-nvim.lua
@@ -0,0 +1,47 @@
+return {
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  dependencies = { {'nvim-tree/nvim-web-devicons'}},
+  config = function()
+    require("dashboard").setup{
+      theme = 'doom',
+      config = {
+        center = {
+          {
+            icon = ' ',
+            desc = 'Find File',
+            key = 'f',
+            key_hl = 'Number',
+            action = function() require("telescope.builtin").find_files() end,
+          },
+          {
+            icon = ' ',
+            desc = 'Open Config',
+            key = 'c',
+            action = ':NvimTreeToggle ~/.config/nvim'
+          },
+          {
+            icon = ' ',
+            desc = 'Open lazy',
+            key = 'l',
+            action = ':Lazy'
+          },
+          {
+            icon = ' ',
+            desc = 'Lazy Update',
+            key = 'u',
+            action = ':Lazy update'
+          },
+          {
+            icon = ' ',
+            desc = 'Quit',
+            key = 'q',
+            action = ':qa'
+          },
+        },
+        vertical_center = true,
+      }
+    }
+  end,
+  enabled = true,
+}
