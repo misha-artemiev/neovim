377f2c19f0cfefc104783c545979e16ab9a5576c
Super User <root@localhost.localdomain>
2025-06-14 01:17:30 +0100

    update

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,25 @@
 return {
   'nvim-telescope/telescope.nvim',
   lazy = false,
-  dependencies = { "nvim-lua/plenary.nvim", },
-  opts = {},
+  dependencies = { "nvim-lua/plenary.nvim", 'nvim-telescope/telescope-dap.nvim', "lpoto/telescope-docker.nvim" },
+  config = function()
+    require("telescope").setup{
+      extensions = {
+        docker = {
+          binary = "podman",
+          log_level = vim.log.levels.INFO,
+        }
+      }
+    }
+    require('telescope').load_extension('dap')
+    require("telescope").load_extension("docker")
+  end,
   keys = {
     { "<leader>ff", function() require("telescope.builtin").find_files() end, silent = true },
     { "<leader>fg", function() require("telescope.builtin").live_grep() end, silent = true },
     { "<leader>fb", function() require("telescope.builtin").buffers() end, silent = true },
     { "<leader>fh", function() require("telescope.builtin").help_tags() end, silent = true },
+    { "<leader>fd", ":Telescope docker<CR>", silent = true},
   },
   enabled = true,
 }
