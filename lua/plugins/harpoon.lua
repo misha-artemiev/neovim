f5ff60eba74f91e6d4ec00615807ebf3a3656b90
Super User <root@localhost.localdomain>
2025-06-17 20:28:57 +0100

    update

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,6 +2,7 @@ return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     lazy = false,
+    dependencies = { "nvim-lua/plenary.nvim" },
     opts = {},
     enabled = vim.g.plugin_harpoon,
 }
