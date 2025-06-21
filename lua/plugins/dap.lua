12173368706d4b7fb3f3ed705bc662a106c8b3bf
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2025-06-21 16:31:34 +0100

    update

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -9,18 +9,18 @@ return {
         -- auto open/close dap-ui
         local dap, dapui = require("dap"), require("dapui")
 
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
+        --dap.listeners.before.attach.dapui_config = function()
+        --    dapui.open()
+        --end
+        --dap.listeners.before.launch.dapui_config = function()
+        --    dapui.open()
+        --end
+        --dap.listeners.before.event_terminated.dapui_config = function()
+        --    dapui.close()
+        --end
+        --dap.listeners.before.event_exited.dapui_config = function()
+        --    dapui.close()
+        --end
 
         -- gdb
         dap.adapters.gdb = {
