32b5413ce9b9e4670ddfa8305d8a99276b69ab3a
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2025-06-24 16:52:47 +0100

    update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -52,6 +52,7 @@ end
 return {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
+    branch = 'master',
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup {
